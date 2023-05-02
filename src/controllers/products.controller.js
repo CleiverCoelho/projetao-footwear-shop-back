dd4921096f66b1b7d5c81697d1108edc6d454d1a
Cherrying55 <cherrying556@protonmail.com>
2023-05-02 15:33:34 -0300

    getproducts

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -17,31 +17,17 @@ export async function getAllproducts (req,res){
 
 
 export async function getProductbyName(req,res){
-    const { authorization } = req.headers;
-  const token = authorization?.replace('Bearer ', '');
-  const usercollection = db.collection("users");
-
-  if(!token) {res.sendStatus(401);}
-
-  const session = await db.collection("sessions").findOne({ token });
-  if (!session) res.sendStatus(401);
-
-	const user = await usercollection.findOne({ 
-		_id: session.userId 
-	})
-
-  if(user) {
+    
+  
     const productscollection = db.collection("products");
     try{
-        const produtos = await productscollection.find({busca: req.query.busca}).toArray();
+        const produtos = await productscollection.find({name: req.body.name}).toArray();
         res.send(produtos)
     }
     catch(error){
         return res.status(500).send(err.message);
     }
-  } else {
-    res.sendStatus(401);
-  }
+  
 }
 
 export async function getProductbyBrand(req,res){
