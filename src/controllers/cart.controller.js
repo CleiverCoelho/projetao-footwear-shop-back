df13456d3277ba1c4364518820a6d7c2f89659fe
Cleiver <creibis10@gmail.com>
2023-05-02 13:45:32 -0300

    feat: criadas opcoes de edicao de quantidade de um produto no caarinho assim como delete

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -34,22 +34,8 @@ export async function getCartProducts(req, res) {
     // const userToken = req.headers.authorization;
     // para integrar com o front
     const userToken = req.headers.authorization?.replace("Bearer ", "");
-    const {quantidades, names} = req.body;
-    const useSchemaParametros = joi.object({
-      quantidades: joi.array().min(1).required(),
-      name: joi.array().required()
-    });
-
-    const validaRequisicao = useSchemaParametros.validate(req.body);
-    if(validaRequisicao.error) {
-      const errors = validaRequisicao.error.details.map((detail) => detail.message);
-      return res.status(422).send(errors);
-    }
-  
-
-    if(quantidades.length !== names.length){
-      res.sendStatus(401)
-    }
+    const {id} = req.params;
+    if(!id) return res.status(401).send("id nao existe");
   
     const userTokenSession = await db.collection('sessions').findOne({token: userToken});
     if(!userTokenSession) return res.status(401).send('usuario nao pode fazer requisicao');
@@ -58,25 +44,14 @@ export async function getCartProducts(req, res) {
       _id: userTokenSession.userId 
     });
   
-  
-
+    const productOnDb = await db.collection('products').findOne({_id : id});
   
    
     try {
-      let produtoscarrinho = [];
-        for(const i in names){
-          const produtox = await db.collection("products").findOne({name: names[i]})
-          if(produtox){
-            let newcartitem = {quantidade: quantidades[i], brand: produtox.brand, valor: produtox.valor, name: produtox.name, image: produtox.image}
-            produtoscarrinho.push(newcartitem);
-          }
-          else{
-            res.sendStatus(401);
-          }
-        }
-        const newCart = {user: user.email, produtos: produtoscarrinho}
+      // adiciona um por vez, quantidade pode ser modificada no carrinho
+        const newCartProduct = {...productOnDb, user: user.email, quantity: 1};
     
-        await db.collection('carts').insertOne(newCart);
+        await db.collection('carts').insertOne(newCartProduct);
         res.status(200).send('produto adicionado no carrinho com suceeso');
       
     } catch (error) {
@@ -84,3 +59,35 @@ export async function getCartProducts(req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function changeProductCart(req, res) {
+  const id = req.params.id;
+  const {quantity} = req.body; 
+  if(!quantity) return res.status(401).send("quantidade deve ser enviado corretamente")
+
+  try {
+    await db.collection("cart").updateOne(
+      {_id: new ObjectId(id)},
+      {$set: quantity}
+    )
+
+    res.send("editado com sucesso")
+  }catch(err){
+    res.status(422).send(err)
+  }
+}
+
+export async function deleteProduct (req, res){
+  const id = req.params.id;
+  if(!id) return ("Id deve ser passado corretamente");
+
+  try {
+    await db.collection('cart').deleteOne({ _id: new ObjectId(id) })
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
+
--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,9 +1,11 @@
-import { getCartProducts, postCart } from "../controllers/cart.controller.js";
+import { getCartProducts, postCart, changeProductCart, deleteProduct } from "../controllers/cart.controller.js";
 import { Router } from "express";
 
 const cartRouter = Router();
 
 cartRouter.get('/cart', getCartProducts );
-cartRouter.post('/cart', postCart);
+cartRouter.post('/cart/:id', postCart);
+cartRouter.put('/cart/:id', changeProductCart);
+cartRouter.delete('/cart/:id', deleteProduct);
 
 export default cartRouter;
